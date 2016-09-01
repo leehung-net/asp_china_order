4a90ac3e93e5f7943ae010acfeacaf2c0d6a3162
toi2192616 <toi2192616@gmail.com>
2016-09-01 14:08:22 +0700

    page thong tin

--- OrderChina/Controllers/HomeController.cs
+++ OrderChina/Controllers/HomeController.cs
@@ -10,26 +10,26 @@ namespace OrderChina.Controllers
         DBContext db = new DBContext();
         public ActionResult Index()
         {
-            if (db.Rates.Any())
-            {
-                var rate = db.Rates.FirstOrDefault();
-                if (rate != null)
-                {
-                    Session["Price"] = rate.Price;
-                    Session["fee1"] = rate.fee1;
-                    Session["fee2"] = rate.fee2;
-                    Session["fee3"] = rate.fee3;
-                }
-            }
-            if (Request.IsAuthenticated)
-            {
-                var userProfile = db.UserProfiles.FirstOrDefault(a => a.Email == User.Identity.Name);
-                if (userProfile != null)
-                {
-                    Session["Name"] = userProfile.Name;
-                    Session["UserType"] = userProfile.UserType;
-                }
-            }
+            //if (db.Rates.Any())
+            //{
+            //    var rate = db.Rates.FirstOrDefault();
+            //    if (rate != null)
+            //    {
+            //        Session["Price"] = rate.Price;
+            //        Session["fee1"] = rate.fee1;
+            //        Session["fee2"] = rate.fee2;
+            //        Session["fee3"] = rate.fee3;
+            //    }
+            //}
+            //if (Request.IsAuthenticated)
+            //{
+            //    var userProfile = db.UserProfiles.FirstOrDefault(a => a.Email == User.Identity.Name);
+            //    if (userProfile != null)
+            //    {
+            //        Session["Name"] = userProfile.Name;
+            //        Session["UserType"] = userProfile.UserType;
+            //    }
+            //}
            
             return View();
         }
@@ -41,6 +41,31 @@ namespace OrderChina.Controllers
         }
 
         public ActionResult Contact()
+        {
+
+            return View();
+        }
+        public ActionResult Introduce()
+        {
+
+            return View();
+        }
+        public ActionResult Banggia()
+        {
+
+            return View();
+        }
+        public ActionResult thanhtoan()
+        {
+
+            return View();
+        }
+        public ActionResult lienhe()
+        {
+
+            return View();
+        }
+        public ActionResult tuyendung()
         {
 
             return View();
